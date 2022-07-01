870c6510a795b79c850807310a1e910f7bdb3897
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-07-01 18:45:39 +0100

    refactor

--- LeadersForFuture/resources/views/admin/user-detail.blade.php
+++ LeadersForFuture/resources/views/admin/user-detail.blade.php
@@ -26,14 +26,9 @@
 
             {{-- Form Info --}}
             <h2 class="dark:text-white text-2xl text-center my-5"> Formulários</h2>
-
             <div class="bg-zinc-200 dark:bg-zinc-700 dark:text-zinc-200 rounded-md p-5 m-3 md:p-10 md:m-10">
-{{--                <button class="block bg-esce px-5 py-2 rounded-md text-white mx-auto"> Inscrever num Projeto</button>--}}
-
                 @php($aux = "")
-
                 @foreach($forms as $form)
-                <a href="{{$form->nome}}">Ir para o formulário</a>
                     {{-- Verify if project header has been displayed --}}
                     @if(!($aux === $form->nome))
                         {{-- Only puts the line and closes the div after thee fists loop as ended --}}
@@ -60,15 +55,12 @@
                         @php($aux = $form->nome)
 
                         <div class="md:grid md:grid-cols-2 bg-zinc-300 dark:bg-zinc-600 dark:text-zinc-200 rounded-md p-5 m-3 md:p-10 ">
-
                             <div>
                                 <p>Ano Letivo: {{ $form->ano_letivo }}</p>
                                 <p> {{ $form->ano_curricular }}º ano {{ $form->semestre + 1 }}º semestre</p>
                             </div>
-
                             <div class="md:text-right">
                                 Estado:
-
                                 @switch($form->estado)
                                     @case(0)
                                         Bloqueado
@@ -89,12 +81,7 @@
                         </div>
                 @endforeach
             </div>
-
-            {{-- TODO - Livewire com a seleção do formulario a preencher --}}
-
             @break
         @endforeach
-
     </div>
-
 @endsection
