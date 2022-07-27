6a71002db9ff6db5f5e59cdbf13cbab4567c9987
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-07-27 19:17:55 +0100

    Changed Project Info

--- LeadersForFuture/resources/views/info-project.blade.php
+++ LeadersForFuture/resources/views/info-project.blade.php
@@ -3,12 +3,25 @@
         <img class="p-1" src="{{ URL('images/esce.png') }}" alt="ESCE Logo">
     </div>
     <div class="text-center">
-        <p>O projeto “Leaders For the Future” tem a sua génese na utilização da metodologia Project Based Learning (PBL)
-            para potenciar nos alunos as competências de desenvolvimento de um plano de negócios (projeto com várias
-            fases) e que envolve a redação de vários textos fundamentados associados à questões relacionadas com as
-            várias fases projeto (questões associadas a formulários a preencher pelos alunos em cada fase do projeto).
-            Neste sentido, os alunos vão preenchendo formulários com várias questões associadas às fases do projeto e os
-            docentes vão validando os textos e interagido com os alunos sem ter de retificar ficheiros e word ou em PDF
-            e evitar versões na troca/interação de documentos entre alunos e docentes.</p>
+        <p>O projeto ESCE-IPVC: <strong> “Leaders For the Future” </strong> tem a sua génese na utilização da
+            metodologia Project Based Learning (PBL) para potenciar nos alunos as competências de desenvolvimento de um
+            plano de negócios (projeto com várias fases) e que envolve a redação de vários textos fundamentados
+            associados à questões relacionadas com as várias fases projeto (questões associadas a formulários a
+            preencher pelos alunos em cada fase do projeto).
+        </p>
+        <br>
+        <p>
+            Para a gestão do projeto “Leaders For the Future” é importante que a plataforma de gestão de fluxo de
+            documentos e de informação e que permita aos alunos submeterem, durante a licenciatura, a informação dos
+            seus projetos para ser avaliada/comentada pelos docentes numa forma centralizada sem troca de e-mails e
+            versões de documentos (word, pdf, etc).
+        </p>
+        <br>
+        <p>
+            Neste sentido, com as funcionalidades disponibilizadas por esta plataforma, os alunos vão preenchendo
+            formulários com várias questões associadas às fases do projeto e os docentes vão validando os textos e
+            interagido com os alunos sem ter de retificar ficheiros e word ou em PDF e evitar versões na troca/interação
+            de documentos entre alunos e docentes.
+        </p>
     </div>
 </div>
