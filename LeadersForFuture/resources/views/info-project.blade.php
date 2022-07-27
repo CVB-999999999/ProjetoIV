4ce24bf72bf3a227c1bd6201466fe1170d55a3fc
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-07-27 16:24:21 +0100

    Update info-project.blade.php

--- LeadersForFuture/resources/views/info-project.blade.php
+++ LeadersForFuture/resources/views/info-project.blade.php
@@ -3,14 +3,12 @@
         <img class="p-1" src="{{ URL('images/esce.png') }}" alt="ESCE Logo">
     </div>
     <div class="text-center">
-        <p>O projeto “Leaders For the Future” tem a sua génese na utilização da metodologia project based learning para
-            potenciar nos alunos as competências de desenvolvimento de um plano de negócios.</p>
-        <p>Este project based learning permite a interação entre diversas UC das licenciaturas e culmina com a UC de
-            Projeto Final no 3º ano.</p>
-        <p>Para a gestão do projeto “Leaders For the Future” é importante criar uma plataforma de gestão de fluxo de
-            documentos e de informação. Uma plataforma que permite aos alunos submeterem, durante a licenciatura, a
-            informação dos seus projetos para ser avaliada/comentada pelos docentes.</p>
-        <p>A plataforma deve permitir criar documentos parciais e um documento final, em pdf. A plataforma deveria
-            utilizar a autenticação federada do IPVC.</p>
+        <p>O projeto “Leaders For the Future” tem a sua génese na utilização da metodologia Project Based Learning (PBL)
+            para potenciar nos alunos as competências de desenvolvimento de um plano de negócios (projeto com várias
+            fases) e que envolve a redação de vários textos fundamentados associados à questões relacionadas com as
+            várias fases projeto (questões associadas a formulários a preencher pelos alunos em cada fase do projeto).
+            Neste sentido, os alunos vão preenchendo formulários com várias questões associadas às fases do projeto e os
+            docentes vão validando os textos e interagido com os alunos sem ter de retificar ficheiros e word ou em PDF
+            e evitar versões na troca/interação de documentos entre alunos e docentes.</p>
     </div>
 </div>
