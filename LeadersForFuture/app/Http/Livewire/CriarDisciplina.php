42adde7d9cecb36786edf5fb5d281303a7322bbb
PauloGarrido17 <79467740+PauloGarrido17@users.noreply.github.com>
2022-07-29 08:15:18 +0100

    r

--- LeadersForFuture/app/Http/Livewire/CriarDisciplina.php
+++ LeadersForFuture/app/Http/Livewire/CriarDisciplina.php
@@ -14,10 +14,12 @@ class CriarDisciplina extends Component
     public $nome;
     public $cod;
     public $sigla;
+    public $curso;
+    public $cursos = [];
 
     public function render()
     {
-       
+        $this->cursos = DB::select("SELECT * FROM Curso");
         return view('livewire.criar-disciplina');
     }
 
@@ -30,14 +32,16 @@ class CriarDisciplina extends Component
         }
 
        
-        if ($this->cod == null || $this->nome == null || $this->sigla == null) {
+        if ($this->cod == null || $this->nome == null || $this->sigla == null || $this->curso == null) {
 
             $this->emit("openModal", "error1", ["message" => 'Os dados que introduziu são inválidos!']);
             return;
         }
         try {
+            //ddd($this->curso);
             DB::insert("INSERT INTO Disciplina (cd_discip, ds_discip, sigla, ativo) Values (?, ?, ?, ?)",
                 [$this->cod, $this->nome, $this->sigla, $ativo]);
+            DB::insert("INSERT INTO cursos_disciplinas(cd_curso,cd_discip) Values (?,?)",[$this->curso, $this->cod]);
         } catch (\Illuminate\Database\QueryException $ex) {
             $this->emit("openModal", "error1", ["message" => 'Os dados que introduziu são inválidos!']);
             return;
--- LeadersForFuture/resources/views/livewire/criar-disciplina.blade.php
+++ LeadersForFuture/resources/views/livewire/criar-disciplina.blade.php
@@ -12,7 +12,15 @@
                     <label for="sigla" class="block font-medium"> Sigla da disciplina </label>
                     <input wire:model="sigla" name="sigla" type="text" class="border border-opacity-50 rounded border-gray-700
                     w-1/2 py-1 px-2 mb-3 dark:bg-zinc-700">
-                </div>
+                    <select wire:model="curso" class="form-control p-3 rounded-md dark:bg-zinc-900 w-1/2 my-2"
+                            name="curso">
+                        <option selected="Curso">Curso</option>
+                        @foreach($cursos as $curso)
+                            <option value="{{ $curso->cd_curso }}">{{ $curso->cd_curso }}
+                                - {{ $curso->nm_curso }}</option>
+                        @endforeach
+                    </select>
+                </div>       
                 <button class="bg-zinc-200 dark:bg-zinc-900 rounded hover:bg-esce hover:text-white px-4 py-2 my-2">
                     Criar Disciplina
                 </button>
