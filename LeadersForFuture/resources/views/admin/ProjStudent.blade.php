7ad1dcb379b98e86804c4878ab9aef3c65b29585
Carlos Vilas Boas <77160567+CVB-999999999@users.noreply.github.com>
2022-07-30 17:52:57 +0100

    Update ProjStudent.blade.php

--- LeadersForFuture/resources/views/admin/ProjStudent.blade.php
+++ LeadersForFuture/resources/views/admin/ProjStudent.blade.php
@@ -2,9 +2,19 @@
 @section('content')
 
     <div class="mx-5 mt-3 dark:text-white text-lg">
-        Listagem de Projetos Afetos aos Alunos
 
-
+        @if(Auth::user()->id_tipoUtilizador == 1)
+            <div class="px-3">
+                Listagem de Projetos Afetos aos Alunos
+            </div>
+        @endif
+        
+         @if(Auth::user()->id_tipoUtilizador == 3)
+            <div class="px-3">
+                Listagem de Projetos Afetos aos Utilizadores
+            </div>
+        @endif
+        
         <div class="relative flex-shrink my-2 mx-auto dark:text-white">
             @livewire('form-table')
         </div>
