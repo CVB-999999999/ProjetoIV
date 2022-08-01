48b75a148bc646118f5a5960b59c3767efdf5830
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-08-01 18:40:00 +0100

    Fix 2

--- LeadersForFuture/app/Http/Livewire/FormProf.php
+++ LeadersForFuture/app/Http/Livewire/FormProf.php
@@ -25,13 +25,13 @@ class FormProf extends Component
     public function render()
     {
         // Gets the Tipo_Formulario
-        try{
-        $this->profnumber = Auth::user()->numero;
+        try {
+            $this->profnumber = Auth::user()->numero;
 //        $this->tpForms = DB::select("exec buscaTipoForm");
 
-        // Gets the projects
-        $this->projetos = DB::select("SELECT p.*  From Projecto p, Utilizador_Projecto up WHERE up.numero_utilizador = ? AND up.id_projecto = p.id", [$this->profnumber]);
-        }catch(\Illuminate\Database\QueryException $ex){
+            // Gets the projects
+            $this->projetos = DB::select("SELECT p.*  From Projecto p, Utilizador_Projecto up WHERE up.numero_utilizador = ? AND up.id_projecto = p.id", [$this->profnumber]);
+        } catch (\Illuminate\Database\QueryException $ex) {
             $this->emit("openModal", "error1", ["message" => 'Ocorreu um erro!']);
             return;
         }
@@ -39,7 +39,8 @@ class FormProf extends Component
         return view('livewire.form-criar');
     }
 
-    public function proj() {
+    public function proj()
+    {
 
         $this->forms = DB::table('Formulario')
             ->where('id_projecto', '=', trim($this->projeto))
@@ -58,18 +59,18 @@ class FormProf extends Component
             $this->emit("openModal", "error1", ["message" => 'O ano letivo não está no formato correto! Deveria ser do tipo ANO ou ANO/ANO']);
         }
 
-        if (/*$this->tpForm == null || */$this->projeto == null || $this->semestre == null || $this->anocurricular == null || $this->ano_letivo[0] == null) {
+        if (/*$this->tpForm == null || */ $this->projeto == null || $this->semestre == null || $this->anocurricular == null || $this->ano_letivo[0] == null) {
 //            return redirect("admin/erro");
             $this->emit("openModal", "error1", ["message" => 'Os dados que introduziu são inválidos!']);
             return;
         }
-        //try{
-        DB::insert("INSERT INTO Formulario (id,estado,id_projecto,ano_letivo,ano_curricular,semestre,titulo) Values (?, ?, ?, ?, ?, ?, ?)",
-            [$this->idform, $this->estado, $this->projeto, $this->ano_letivo[0], $this->anocurricular, $this->semestre, $this->titulo]);
-        //}catch(\Illuminate\Database\QueryException $ex){
-        //    $this->emit("openModal", "error1", ["message" => 'Ocorreu um erro!']);
-          //  return;
-        //}
+        try {
+            DB::insert("INSERT INTO Formulario (id,estado,id_projecto,ano_letivo,ano_curricular,semestre,titulo) Values (?, ?, ?, ?, ?, ?, ?)",
+                [$this->idform, $this->estado, $this->projeto, $this->ano_letivo[0], $this->anocurricular, $this->semestre, $this->titulo]);
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $this->emit("openModal", "error1", ["message" => 'Ocorreu um erro!']);
+            return;
+        }
 
         return redirect("prof/proj/");
     }
--- LeadersForFuture/resources/views/forms/form-selection.blade.php
+++ LeadersForFuture/resources/views/forms/form-selection.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="w-full min-h-screen">
+    <div class="w-full min-h-screen">
         {{-- Professor --}}
         @if(Auth::user()->id_tipoUtilizador == 3)
             <div class="md:grid md:grid-cols-2 m-2 md:m-5">
-                <div class="md:text-right my-2 grid grid-cols-2 md:flex md:ml-auto">
+                <div class="md:text-right my-2 md:ml-auto">
+                    <div><strong class="dark:text-white text-lg">Listagem de formulários no sistema</strong></div>
                     <div>
-                        <strong class="dark:text-white text-lg">Listagem de formulários no sistema</strong>
-                        <a href="/admin/addform/" class="block bg-zinc-200 dark:bg-zinc-900 rounded hover:bg-esce
+                        <a href="/admin/addform/" class="bg-zinc-200 dark:bg-zinc-900 rounded hover:bg-esce
                                 hover:text-white px-4 py-2 dark:text-white mx-2">
                             <span class="material-symbols-outlined align-middle h-7">add</span>
                             Criar Formulário
@@ -16,13 +16,13 @@
                     </div>
                 </div>
             </div>
-            @endif
-            @if(Auth::user()->id_tipoUtilizador == 1)
+        @endif
+        @if(Auth::user()->id_tipoUtilizador == 1)
             <div class="md:grid md:grid-cols-2 m-2 md:m-5">
-                <div class="md:text-right my-2 grid grid-cols-2 md:flex md:ml-auto">
-                    <div>
-                        <strong class="dark:text-white text-lg">Listagem de formulários do professor</strong>
-                        <a href="/prof/addform/" class="block bg-zinc-200 dark:bg-zinc-900 rounded hover:bg-esce
+                <div class="md:text-right my-2 md:ml-auto">
+                    <div><strong class="dark:text-white text-lg">Listagem de formulários do professor</strong></div>
+                    <div class="my-2 text-center ">
+                        <a href="/prof/addform/" class="bg-zinc-200 dark:bg-zinc-900 rounded hover:bg-esce
                                 hover:text-white px-4 py-2 dark:text-white mx-2">
                             <span class="material-symbols-outlined align-middle h-7">add</span>
                             Criar Formulário
@@ -30,7 +30,7 @@
                     </div>
                 </div>
             </div>
-            @endif
+        @endif
         @livewire('form-select-std')
     </div>
 @endsection
